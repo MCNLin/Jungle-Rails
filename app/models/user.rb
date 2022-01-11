d411b015cba07a61aafa25f073d47547c3cfb286
Mei Nghiem-Lin <mcnghiem@gmail.com>
2022-01-11 01:09:39 +0000

    update to pass spec test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ActiveRecord::Base
 
   has_secure_password
-  validates :email, uniqueness: { case_sensitive: false} 
+  validates :email, uniqueness: { case_sensitive: false}, presence:true 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :password, presence: true, confirmation: true, length: { minimum: 5 }
+  validates :password_confirmation, presence: true
+ 
   
 end
 
