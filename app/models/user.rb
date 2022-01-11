b7818e3e66dd7750153eed0647fa30645f3ef925
Mei Nghiem-Lin <mcnghiem@gmail.com>
2022-01-11 02:52:18 +0000

    create class method to pass specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,13 @@ class User < ActiveRecord::Base
   validates :password, presence: true, confirmation: true, length: { minimum: 5 }
   validates :password_confirmation, presence: true
  
-  
+  def self.authenticate_with_credentials(email, password)
+    @user = User.find_by(email: email.strip.downcase)  
+    if @user && @user.authenticate(password)
+      return @user
+    else
+      return nil
+    end
+  end
 end
 
